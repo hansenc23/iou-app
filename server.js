e07fec283eb5860f7d234605d007972a29f1be7c
hansenc23 <hansenc216@gmail.com>
2020-08-22 23:05:15 +1000

    test deployment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   });
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
