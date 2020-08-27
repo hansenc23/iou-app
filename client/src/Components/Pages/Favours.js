62b97f68ce1a68401366271356060a43d03a3b0b
gianriyanto <li.griyanto@gmail.com>
2020-08-27 21:33:08 +1000

    Setup project folder structure for main pages of the IOU application.

---
+++ client/src/Components/Pages/Favours.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Favours extends Component {
+
+    render() {
+        return (
+            <div>
+                Favours Page
+            </div>
+        )
+    }
+}
---
+++ client/src/Components/Pages/Leaderboard.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Leaderboard extends Component {
+
+    render() {
+        return (
+            <div>
+                Leaderboard Page
+            </div>
+        )
+    }
+}
