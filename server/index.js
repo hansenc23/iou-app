ec4f868081ad76fcd3b605405eab835ef6eaa9fb
hansenc23 <hansenc216@gmail.com>
2020-08-30 15:00:58 +1000

    set up mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 
 //CORS
@@ -16,5 +17,14 @@ const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
+
+mongoose
+  .connect('mongodb+srv://dbadmin:test123@iou-db.377ul.mongodb.net/iou-app?retryWrites=true&w=majority', { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 module.exports = server;
