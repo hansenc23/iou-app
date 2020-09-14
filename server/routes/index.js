9c9ae1876d19e4ccc3a4514fde2f5677916833b7
Hansen Christian <47245061+hansenc23@users.noreply.github.com>
2020-09-14 18:12:43 +1000

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,8 +6,8 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     res.json({
-        username: 'Hansen',
-        password: 'test123',
+        username: 'HansenC',
+        password: 'test123456',
     });
 });
 
