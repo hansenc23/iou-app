f97f906114f376af0c15d4a5ec96a75cc8747737
hansenc23 <hansenc216@gmail.com>
2020-09-14 15:51:36 +1000

    resolve conflicts on Navbar.js

--- client/src/navbar/Navbar.js
+++ client/src/navbar/Navbar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
@@ -80,57 +79,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-=======
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
-import Login from "../pages/loginPage/LoginPage";
-
-class Navbar extends Component {
-
-    render() {
-        return (
-            <div id="navbar">
-                <div className="page_links">
-                    <nav role="navigation">
-                        <div id="menuToggle">
-                            <input type="checkbox" className=""/>
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            <ul id="menu">
-                                <div className="page_links">
-                                    <Link to="/requests">Requests</Link>
-                                </div>
-                                <div className="page_links">
-                                    <Link to="/favours">Favours</Link>
-                                </div>
-                                <div className="page_links">
-                                    <Link to="/leaderboard">Leaderboard</Link>
-                                </div>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
-
-                <div className="logo">
-                    IOWEYOU
-                </div>
-
-                <div className="authentication_links">
-                    <div className="login_link">
-                        <Link to="/login">Login</Link>
-                    </div>
-                    <div className="signup_link">
-                        <button className= "signup_link_label">
-                            <Link to="/signup">Sign Up</Link>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
-
-export default Navbar;
->>>>>>> aed91269755c6aea0947938a175d679552608bfd
