d370bebb6290c0076f7f14e50381143d1d459ceb
hansenc23 <hansenc216@gmail.com>
2020-10-05 18:06:54 +1100

    call getUser function on request page

--- client/src/pages/requestsPage/RequestsPage.js
+++ client/src/pages/requestsPage/RequestsPage.js
@@ -1,46 +1,52 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useContext } from 'react';
 import './RequestsPage.css';
 import RequestsList from '../../Components/requestList/RequestList';
 import RequestDetail from '../../Components/requestDetail/RequestDetail';
-import axios from "axios";
+import { AuthContext } from '../../context/AuthContext';
+import axios from 'axios';
 
 const RequestsPage = () => {
+  const { isAuth, setIsAuth, user, setUser, getUser } = useContext(AuthContext);
 
   const [selectedRequestID, setSelectedRequestID] = useState();
 
   const [requestData, setRequestData] = useState([]);
 
   const selectRequestId = useCallback((selectedID) => {
-      setSelectedRequestID(selectedID);
+    setSelectedRequestID(selectedID);
   }, []);
 
-    useEffect(() => {
-        axios
-            .get(`${process.env.REACT_APP_API_URL}/request/get_all`)
-            .then((response) => {
-                if (response.data.success) {
-                    setRequestData(response.data.requests);
-                    setSelectedRequestID(response.data.requests[0]._id);
-                } else {
-                    console.log("Failed to get request data");
-                }
-            }).catch(error => {
-            console.log(error);
-        })
-    },  []);
+  useEffect(() => {
+    getUser();
+  }, []);
 
-    const selectedRequest = requestData.find( item => item._id === selectedRequestID);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/request/get_all`)
+      .then((response) => {
+        if (response.data.success) {
+          setRequestData(response.data.requests);
+          setSelectedRequestID(response.data.requests[0]._id);
+        } else {
+          console.log('Failed to get request data');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const selectedRequest = requestData.find((item) => item._id === selectedRequestID);
 
   return (
-      <div id='RequestsPage' className=''>
-          <RequestsList
-              requestData={requestData}
-              selectRequestId={selectRequestId}
-          />
-          {(requestData && selectedRequest) ?
-              <RequestDetail selectedRequest={selectedRequest}/> :
-              <div className = "empty_RequestList"> Request Database is Empty </div> }
-      </div>
+    <div id='RequestsPage' className=''>
+      <RequestsList requestData={requestData} selectRequestId={selectRequestId} />
+      {requestData && selectedRequest ? (
+        <RequestDetail selectedRequest={selectedRequest} />
+      ) : (
+        <div className='empty_RequestList'> Request Database is Empty </div>
+      )}
+    </div>
   );
 };
 
