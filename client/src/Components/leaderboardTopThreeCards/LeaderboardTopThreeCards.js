93782a7016e3f0eb0efde360d4231fc8132f28f1
gianriyanto <li.griyanto@gmail.com>
2020-10-17 02:23:35 +1100

    Fixed bug on LeaderboardTopThreeCards.js where top 3 users all display "1st" place.

--- client/src/Components/leaderboardTopThreeCards/LeaderboardTopThreeCards.js
+++ client/src/Components/leaderboardTopThreeCards/LeaderboardTopThreeCards.js
@@ -3,12 +3,25 @@ import "./LeaderboardTopThreeCards.css";
 import Spinner from "../Spinner";
 
 function LeaderboardCard({completedRequestData, isLoading}) {
+
+    function getRankingLabel(index) {
+        if (index === 0) {
+            return "1st";
+        }
+        else if (index === 1) {
+            return "2nd";
+        }
+        else {
+            return "3rd";
+        }
+    }
+
     const topThreeUsers = completedRequestData.slice(0, 3).map(users => {
         return (
             <div key={users._id} className="top_3_card">
                 <div className="top_3_header">
                     <div className="ranking_container">
-                        <span className='ranking_label'> 1st </span>
+                        <span className='ranking_label'> {getRankingLabel(completedRequestData.indexOf(users))} </span>
                     </div>
                     <div className="top_3_user_name">
                         {users.user_data[0].firstName} {users.user_data[0].lastName}
