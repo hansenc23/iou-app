fa22b6068d0da424163e63534f91ea4699099708
gianriyanto <li.griyanto@gmail.com>
2020-10-19 14:23:39 +1100

    UI fixes and adjustments on  layout of the requests page

--- client/src/Components/requestList/RequestList.js
+++ client/src/Components/requestList/RequestList.js
@@ -96,7 +96,7 @@ const RequestsList = ({ requestData, selectRequestId, setSelectedRequestID, isLo
                 </div>
               </div>
             );
-          }) : (<h3 className="empty_request">No request available</h3>)
+          }) : (<h3 className="empty_request">No requests found</h3>)
         )}
       </div>
     </div>
